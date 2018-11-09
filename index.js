14e157fd798e13bf97bffabe0633c653b3575a6a
Prasanna Loganathar <pvl@prasannavl.com>
2018-11-09 16:41:24 +0530

    add build process

--- index.js
+++
@@ -1,3 +0,0 @@
-export { LiteElementBase, LiteComponentBase } from "./lib/base";
-export { LiteElement, LiteComponent, LiteElementFn } from "./lib/lit";
-export { register, registerTag } from "./lib/util";
