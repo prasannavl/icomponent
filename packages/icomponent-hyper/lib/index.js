d2ca12ae340f6e1981660e96fd3ff7acee21005a
Prasanna Loganathar <pvl@prasannavl.com>
2018-11-11 02:05:59 +0530

    simplify hyper bind

--- packages/icomponent-hyper/lib/index.js
+++ packages/icomponent-hyper/lib/index.js
@@ -7,30 +7,14 @@ export { bind, wire } from "hyperhtml";
 export const html = (...args) => args;
 
 export class IElement extends CoreIElement {
-    constructor() {
-        super();
-        this._hyperRoot = null;
-    }
-
     _render() {
-        if (this._hyperRoot === null) {
-            this._hyperRoot = bind(this.getRenderRoot());
-        }
-        this._hyperRoot(...this.view());
+        bind(this.getRenderRoot())(...this.view());
     }
 }
 
 export class IComponent extends CoreIComponent {
-    constructor() {
-        super();
-        this._hyperRoot = null;
-    }
-
     _render() {
-        if (this._hyperRoot === null) {
-            this._hyperRoot = bind(this.getRenderRoot());
-        }
-        this._hyperRoot(...this.view());
+        bind(this.getRenderRoot())(...this.view());
     }
 }
 
